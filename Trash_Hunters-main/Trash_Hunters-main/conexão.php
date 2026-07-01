e8b4b789315e4e85e2ab1bca3cc0c546874f6804
bfs-2 <bfs4@discente.ifpe.edu.br>
2026-07-01 12:46:00 -0300

    Refactor database connection to use mysqli

--- Trash_Hunters-main/Trash_Hunters-main/conexão.php
+++ Trash_Hunters-main/Trash_Hunters-main/conexão.php
@@ -1,16 +1,13 @@
 <?php
 
-// config/database.php
-$db_host = 'localhost';
-$db_name = 'hospital_bd';
-$db_user = 'root';
-$db_pass = '0000';
+$host = "localhost";
+$usuario = "root";
+$senha = "";
+$database = "login";
 
-try {
-    $conn = new PDO("mysql:host=$db_host;dbname=$db_name", $db_user, $db_pass);
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-} catch (PDOException $e) {
-    die("Connection failed: " . $e->getMessage());
+$mysqli = new mysqli($host, $usuario, $senha, $database);
+
+if ($mysqli->connect_errno) {
+    die("Falha na conexao: " . $mysqli->connect_error);
 }
-
 ?>
